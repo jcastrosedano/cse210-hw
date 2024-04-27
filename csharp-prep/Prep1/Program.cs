5cb7e7545f0725118c94b732a44eac7c631d4d42
Jorge <jcastro.sedano@gmail.com>
2024-04-27 01:15:27 -0500

    Excercise completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
