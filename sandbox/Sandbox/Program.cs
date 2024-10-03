c27b384c317fffda565d67acab5024c79cc6270d
Jorge <jcastro.sedano@gmail.com>
2024-10-03 00:23:32 -0500

    Testing connection

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,10 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Hello Sandbox World!");
+    //}
+
+    
 }
